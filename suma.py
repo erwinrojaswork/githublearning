69d461bb40d740089822ba503e0bf7b639c7941a
erwinrojas <erwin.rojas@outlook.es>
2026-02-18 17:58:08 -0400

    Adicion de logica para suma

--- suma.py
+++ suma.py
@@ -1,2 +1,8 @@
 #Esta rutina es para realizar sumas entre 2 numeros enteros 
 #Tiene propositos educativos
+a=0
+b=0
+a=int(input("Ingresa el valor de a: "))
+b=int(input("Ingresa el valor de a: "))
+suma=a+b
+print(f"La suma es: {suma}")
